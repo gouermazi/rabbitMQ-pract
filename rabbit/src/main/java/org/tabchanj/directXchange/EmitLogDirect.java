10fdc0a2eb1204fa48c1c722f1990cad4dffbecf
tab <tabchanj@gmail.com>
2018-02-07 15:24:47 +0800

    direct message

---
+++ rabbit/src/main/java/org/tabchanj/directXchange/EmitLogDirect.java
@@ -0,0 +1,55 @@
+package org.tabchanj.directXchange;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.util.concurrent.TimeoutException;
+
+/**
+ * created by tab chan on 2018/02/07
+ */
+public class EmitLogDirect {
+    private static final String EXCHANGE_NAME = "direct_logs";
+
+    public static void main(String[] argv)
+            throws java.io.IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+
+        String severity = getSeverity(argv);
+        String message = getMessage(argv);
+
+        channel.basicPublish(EXCHANGE_NAME, severity, null, message.getBytes());
+        System.out.println(" [x] Sent '" + severity + "':'" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+    private static String getSeverity(String[] strings){
+        if (strings.length < 1)
+            return "info";
+        return strings[0];
+    }
+
+    private static String getMessage(String[] strings){
+        if (strings.length < 1)
+            return "Hello World!";
+        return joinStrings(strings, " ");
+    }
+
+    private static String joinStrings(String[] strings, String delimiter) {
+        int length = strings.length;
+        if (length == 0) return "";
+        StringBuilder words = new StringBuilder(strings[0]);
+        for (int i = 1; i < length; i++) {
+            words.append(delimiter).append(strings[i]);
+        }
+        return words.toString();
+    }
+}
